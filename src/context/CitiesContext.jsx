95ed88057ea7e589f8698902a4b975a23f12761b
mevdenic <mevdenic@gmail.com>
2024-01-19 14:11:15 +0100

    test

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -2,6 +2,45 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const CitiesContext = createContext();
 
+const tempData = [
+    {
+        cityName: "Lisbon",
+        country: "Portugal",
+        emoji: "🇵🇹",
+        date: "2027-10-31T15:59:59.138Z",
+        notes: "My favorite city so far!",
+        position: {
+            lat: 38.727881642324164,
+            lng: -9.140900099907554,
+        },
+        id: 73930385,
+    },
+    {
+        cityName: "Madrid",
+        country: "Spain",
+        emoji: "🇪🇸",
+        date: "2027-07-15T08:22:53.976Z",
+        notes: "",
+        position: {
+            lat: 40.46635901755316,
+            lng: -3.7133789062500004,
+        },
+        id: 17806751,
+    },
+    {
+        cityName: "Berlin",
+        country: "Germany",
+        emoji: "🇩🇪",
+        date: "2027-02-12T09:24:11.863Z",
+        notes: "Amazing 😃",
+        position: {
+            lat: 52.53586782505711,
+            lng: 13.376933665713324,
+        },
+        id: 98443197,
+    },
+];
+
 const BASE_URL = "http://localhost:8000/cities";
 
 export function CitiesProvider({ children }) {
@@ -10,19 +49,7 @@ export function CitiesProvider({ children }) {
     const [currentCity, setCurrentCity] = useState({});
 
     useEffect(function () {
-        async function fetchCities() {
-            try {
-                setIsLoading(true);
-                const res = await fetch(`${BASE_URL}`);
-                const data = await res.json();
-                setCities(data);
-            } catch {
-                alert("There was an error loading data.");
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchCities();
+        setCities(tempData);
     }, []);
 
     async function getCity(id) {
